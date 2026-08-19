3bc971d59effca511f61c2f9d7222e5bcd2a8f55
jiabo <843051627@qq.com>
2026-08-19 15:22:36 +0800

    refactor: rename test functions and variables

--- internal/service/bug02_test.go
+++ internal/service/bug02_test.go
@@ -9,7 +9,7 @@ import (
 	"mine-vent/internal/store"
 )
 
-func TestBug02_CheckThresholdsPollutesCache(t *testing.T) {
+func TestMV02_CheckThresholdsPollutesCache(t *testing.T) {
 	dbPath := filepath.Join(t.TempDir(), "test.db")
 	s, err := store.NewStore(dbPath)
 	if err != nil {
