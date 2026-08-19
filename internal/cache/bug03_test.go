d8f4ec7fd4bd2ccd0d15fc5766e5c6bf1cea55ca
jiabo <843051627@qq.com>
2026-08-19 14:58:22 +0800

    add feature: concurrency RLock write concurrent access issue

---
+++ internal/cache/bug03_test.go
@@ -0,0 +1,36 @@
+package cache
+
+import (
+	"sync"
+	"testing"
+
+	"mine-vent/internal/model"
+)
+
+func TestBug03_ConcurrentCacheUpdateDataRace(t *testing.T) {
+	rc := NewReadingCache()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			rc.Update("sensor-1", &model.Reading{
+				ID: "r", SensorID: "sensor-1", Value: float64(n),
+			})
+		}(i)
+	}
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			rc.Get("sensor-1")
+		}()
+	}
+	wg.Wait()
+
+	r := rc.Get("sensor-1")
+	if r == nil {
+		t.Error("expected non-nil reading after updates")
+	}
+}
